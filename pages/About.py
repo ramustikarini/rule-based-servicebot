689824ceb9090bfe3c5dc46df0df5e8bf47f88ba
ramustikarini <ra.mustikarini@gmail.com>
2024-02-11 15:44:13 +0700

    fix error + add rules + edit about

--- pages/About.py
+++ pages/About.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title(':blue[$About$ $This$ $Bot$]')
 #st.divider()
 with st.container(border=True):
-    st.markdown('''I'm a service bot to help you get the information needed regarding our dental clinic. I can only 
+    st.markdown('''I'm a service bot to help you get the information need regarding our dental clinic. I can only 
             serve and answer questions about our clinic and services because I'm a rule-based chatbot.
             I have limited knowledge, but my creator will keep on feeding me informations and new command if necessary. 
             I'm sorry for the inconvinience if there are some questions I can't answer.''')
@@ -14,6 +14,9 @@ data = {
     "Description": ["Information of the clinic's schedule","Information for registration and appointment","Information of the clinic's location","Information of the available services and treatments"]
 }
 
-"The informations you can get from me are:"
+"Here are some main informations you can get from me:"
 df = pd.DataFrame(data)
 st.dataframe(df, hide_index=True, use_container_width=True)
+
+st.text("Project by: Rahma Amanda Mustikarini")
+st.text("Final Project SanberCampus Fullstack Data Science")
